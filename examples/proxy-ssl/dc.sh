9af3f2c928db77d9feb6de390e3fe32b6e95ef6e
John Wregglesworth <wregglej@iplantcollaborative.org>
2017-07-06 09:08:19 -0700

    Enable upstream SSL verification

--- examples/proxy-ssl/dc.sh
+++ examples/proxy-ssl/dc.sh
@@ -12,5 +12,7 @@ export HOST_PROXY_SSL_KEY_PATH=
 export HOST_PROXY_SSL_CERT_PATH=
 export PROXY_SSL_KEY_PATH=
 export PROXY_SSL_CERT_PATH=
+export HOST_NGINX_CA_CERT_PATH=
+export NGINX_CA_CERT_PATH=/etc/ssl/trusted.crt
 
 docker-compose -f docker-compose.yml "$@"
